6028aea9b135311212d1818803f94de815b4bb45
Nayara Vasconcelos <nayaravasconcelos.dev@gmail.com>
2022-07-05 16:39:00 -0300

    feat: adiciona model BlogPost

---
+++ src/database/models/blogPost.js
@@ -0,0 +1,21 @@
+module.exports = (sequelize, DataTypes) => {
+  const BlogPost = sequelize.define('BlogPost', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    title: DataTypes.STRING,
+    content: DataTypes.STRING,
+    userId: { type: DataTypes.INTEGER, foreignKey: true },
+    published: { type: DataTypes.DATE, defaultValue: DataTypes.NOW },
+    updated: { type: DataTypes.DATE, defaultValue: DataTypes.NOW }
+  },
+  {
+    timestamps: false,
+    tableName: 'BlogPosts',
+  });
+
+  BlogPost.associate = (models) => {
+    BlogPost.belongsTo(models.User,
+      { foreignKey: 'userId', as: 'users' });
+  };
+
+  return BlogPost;
+};

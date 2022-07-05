4638a325706cbf6025681fd9db70c86562a41b02
Nayara Vasconcelos <nayaravasconcelos.dev@gmail.com>
2022-07-05 00:11:53 -0300

    feat: adiciona a rota GET /user/:id

--- src/routes/user/index.js
+++ src/routes/user/index.js
@@ -3,11 +3,12 @@ const rescue = require('express-rescue');
 
 const { validateToken } = require('../../middlewares/authMiddleware');
 const { validateUser } = require('../../middlewares/userMiddlewares');
-const { create, getAll } = require('../../controllers/userController');
+const { create, getAll, getById } = require('../../controllers/userController');
 
 const userRouter = express.Router();
 
 userRouter.get('/', validateToken, rescue(getAll));
+userRouter.get('/:id', validateToken, rescue(getById));
 userRouter.post('/', validateUser, rescue(create));
 
 module.exports = userRouter;

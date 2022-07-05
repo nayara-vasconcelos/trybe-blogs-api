78ae5b53ca8c568a9ae01f66efb1e8d45ddd9335
Nayara Vasconcelos <nayaravasconcelos.dev@gmail.com>
2022-07-05 18:15:02 -0300

    feat: adiciona middleware para POST /categories

--- src/controllers/categoriesController.js
+++ src/controllers/categoriesController.js
@@ -1,11 +1,19 @@
 const categoriesService = require('../services/categoriesService');
-const { OK } = require('../constants/statusCodes');
+const { OK, CREATED } = require('../constants/statusCodes');
 
 const getAll = async (_req, res, _next) => {
   const categories = await categoriesService.getAll();
   return res.status(OK).json(categories);
 };
 
+const create = async (req, res, _next) => {
+  const { name } = req.body;
+  const category = await categoriesService.create(name);
+
+  return res.status(CREATED).json(category);
+};
+
 module.exports = {
   getAll,
+  create,
 };

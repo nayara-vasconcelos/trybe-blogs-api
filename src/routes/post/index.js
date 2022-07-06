8a65b8fb430e0cc73312557c8cdfc121ee95b8ec
Nayara Vasconcelos <nayaravasconcelos.dev@gmail.com>
2022-07-06 12:01:13 -0300

    feat: adiciona a rota POST /post

--- src/routes/post/index.js
+++ src/routes/post/index.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const rescue = require('express-rescue');
 
+const { validatePost } = require('../../middlewares/postMiddleware');
 const { validateToken } = require('../../middlewares/authMiddleware');
-const { getAll, getById, deleteById } = require('../../controllers/postController');
+const { getAll, getById, deleteById, create } = require('../../controllers/postController');
 
 const postRouter = express.Router();
 
 postRouter.get('/', validateToken, rescue(getAll));
 postRouter.get('/:id', validateToken, rescue(getById));
 postRouter.delete('/:id', validateToken, rescue(deleteById));
+postRouter.post('/', validateToken, validatePost, rescue(create));
 
 module.exports = postRouter;

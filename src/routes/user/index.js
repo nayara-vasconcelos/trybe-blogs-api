b7e802ae0a5e42c18e700e7c916ca5ecc60b8889
Nayara Vasconcelos <nayaravasconcelos.dev@gmail.com>
2022-07-05 22:23:20 -0300

    feat: adiciona a rota DELETE /user/me

--- src/routes/user/index.js
+++ src/routes/user/index.js
@@ -3,12 +3,13 @@ const rescue = require('express-rescue');
 
 const { validateToken } = require('../../middlewares/authMiddleware');
 const { validateUser } = require('../../middlewares/userMiddlewares');
-const { create, getAll, getById } = require('../../controllers/userController');
+const { create, getAll, getById, deleteById } = require('../../controllers/userController');
 
 const userRouter = express.Router();
 
 userRouter.get('/', validateToken, rescue(getAll));
 userRouter.get('/:id', validateToken, rescue(getById));
 userRouter.post('/', validateUser, rescue(create));
+userRouter.delete('/me', validateToken, rescue(deleteById));
 
 module.exports = userRouter;

21dadab77e503705c1f085c35224edc5a531eb68
Nayara Vasconcelos <nayaravasconcelos.dev@gmail.com>
2022-07-05 21:21:28 -0300

    feat: adiciona funcao para buscar posts

---
+++ src/services/postService.js
@@ -0,0 +1,16 @@
+const { BlogPost, User, Category } = require('../database/models');
+
+const getAll = async () => {
+  const posts = await BlogPost.findAll({
+    include: [
+      { model: User, as: 'user', attributes: { exclude: ['password'] } },
+      { model: Category, as: 'categories', through: { attributes: [] } },
+    ],
+  });
+
+  return (posts);
+};
+
+module.exports = {
+  getAll,
+};

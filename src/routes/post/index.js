f3d142f0b5717c6a99d04c780a8c1c6b9fa636ba
Nayara Vasconcelos <nayaravasconcelos.dev@gmail.com>
2022-07-06 09:33:10 -0300

    feat: adiciona a rota DELETE /post/:id

--- src/routes/post/index.js
+++ src/routes/post/index.js
@@ -2,11 +2,12 @@ const express = require('express');
 const rescue = require('express-rescue');
 
 const { validateToken } = require('../../middlewares/authMiddleware');
-const { getAll, getById } = require('../../controllers/postController');
+const { getAll, getById, deleteById } = require('../../controllers/postController');
 
 const postRouter = express.Router();
 
 postRouter.get('/', validateToken, rescue(getAll));
 postRouter.get('/:id', validateToken, rescue(getById));
+postRouter.delete('/:id', validateToken, rescue(deleteById));
 
 module.exports = postRouter;

2d4d0ced256e84b1fb9f614085242a5cfe8dd172
Nayara Vasconcelos <nayaravasconcelos.dev@gmail.com>
2022-07-05 21:39:32 -0300

    feat: adiciona funcao para buscar post por id

--- src/services/postService.js
+++ src/services/postService.js
@@ -1,4 +1,10 @@
 const { BlogPost, User, Category } = require('../database/models');
+const { notFound } = require('../constants/statusCodeTypes');
+
+const postNotFoundError = {
+  code: notFound,
+  message: 'Post does not exist',
+};
 
 const getAll = async () => {
   const posts = await BlogPost.findAll({
@@ -11,6 +17,21 @@ const getAll = async () => {
   return (posts);
 };
 
+const getById = async (id) => {
+  const post = await BlogPost.findOne({
+    where: { id: parseInt(id, 10) },
+    include: [
+      { model: User, as: 'user', attributes: { exclude: ['password'] } },
+      { model: Category, as: 'categories', through: { attributes: [] } },
+    ],
+  });
+
+  if (!post) { return ({ error: postNotFoundError }); }
+
+  return (post);
+};
+
 module.exports = {
   getAll,
+  getById,
 };

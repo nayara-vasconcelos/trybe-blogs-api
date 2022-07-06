c1ca3a4aae74c623c5cbba572176ec02cd39c460
Nayara Vasconcelos <nayaravasconcelos.dev@gmail.com>
2022-07-05 21:40:00 -0300

    feat: adiciona middleware para GET /post/:id

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -6,6 +6,15 @@ const getAll = async (_req, res, _next) => {
   return res.status(OK).json(posts);
 };
 
+const getById = async (req, res, next) => {
+  const { id } = req.params;
+  const post = await postService.getById(id);
+  if (post.error) { return next(post.error); }
+
+  return res.status(OK).json(post);
+};
+
 module.exports = {
   getAll,
+  getById,
 };
